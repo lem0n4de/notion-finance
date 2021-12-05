7b36adc03b713e42a327d563b35b35b88b239225
Yunus Emre ÖRCÜN <yunus.emre.orcun@outlook.com>
2021-12-05 18:38:47 +0300

    Add UserDbContext

---
+++ NotionFinance/Data/UserDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotionFinance.Models;
+
+namespace NotionFinance.Data;
+
+public class UserDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+}
--- NotionFinance/Program.cs
+++ NotionFinance/Program.cs
@@ -1,6 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using NotionFinance.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<UserDbContext>(options =>
+    options.UseSqlite($"Data Source={builder.Configuration["Sqlite:Users"]}"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
