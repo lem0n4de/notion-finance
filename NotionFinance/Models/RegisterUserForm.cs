658eb4865bd30732db7e193ef9d490f3ae3dffa4
Yunus Emre ÖRCÜN <yunus.emre.orcun@outlook.com>
2021-12-05 19:42:15 +0300

    Add a user registration form model

---
+++ NotionFinance/Models/RegisterUserForm.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Build.Framework;
+
+namespace NotionFinance.Models;
+
+public class RegisterUserForm
+{
+    public string? FirstName { get; set; }
+    public string? LastName { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
