6dc5275e0e18f5c88a25064163a7f6b376ed8ee7
Yunus Emre ÖRCÜN <yunus.emre.orcun@outlook.com>
2021-12-05 19:41:54 +0300

    Fix UserDbContext

--- NotionFinance/Data/UserDbContext.cs
+++ NotionFinance/Data/UserDbContext.cs
@@ -6,4 +6,6 @@ namespace NotionFinance.Data;
 public class UserDbContext : DbContext
 {
     public DbSet<User> Users { get; set; }
+
+    public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
 }
