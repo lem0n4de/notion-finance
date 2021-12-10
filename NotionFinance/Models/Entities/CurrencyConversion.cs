4d33eab5a23fdb3f97ef5f9d0ee3948bf1e230cb
Yunus Emre ÖRCÜN <yunus.emre.orcun@outlook.com>
2021-12-10 14:48:09 +0300

    Add IForexService and related classes

---
+++ NotionFinance/Models/Entities/CurrencyConversion.cs
@@ -0,0 +1,8 @@
+﻿namespace NotionFinance.Models;
+
+public class CurrencyConversion
+{
+    public Currency From;
+    public List<Currency> Rates;
+    public DateTime Date;
+}
---
+++ NotionFinance/Services/Forex/IForexService.cs
@@ -0,0 +1,15 @@
+﻿using Notion.Client;
+using NotionFinance.Models;
+
+namespace NotionFinance.Services.Forex;
+
+public interface IForexService
+{
+    public Task<CurrencyConversion> ConvertAsync(Currency from);
+    public Task<CurrencyConversion> ConvertAsync(Currency from, Currency to);
+    public Task<CurrencyConversion> ConvertAsync(Currency from, List<Currency> currencies);
+    public Task<CurrencyConversion> ConvertOnDateAsync(Currency from, DateTime date);
+    public Task<CurrencyConversion> ConvertOnDateAsync(Currency from, Currency to, DateTime date);
+    public Task<CurrencyConversion> ConvertOnDateAsync(Currency from, List<Currency> currencies, DateTime date);
+    public Task<IEnumerable<Currency>> GetSupportedCurrenciesAsync();
+}
