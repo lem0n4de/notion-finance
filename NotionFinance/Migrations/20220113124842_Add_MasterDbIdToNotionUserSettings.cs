118c14afe9f0c36d56350181034fc097ba9ef9a9
Yunus Emre ÖRCÜN <yunus.emre.orcun@outlook.com>
2022-01-13 16:01:28 +0300

    Add Migrations

---
+++ NotionFinance/Migrations/20220113124842_Add_MasterDbIdToNotionUserSettings.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NotionFinance.Migrations
+{
+    public partial class Add_MasterDbIdToNotionUserSettings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "MasterDatabaseId",
+                table: "NotionUserSettings",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MasterDatabaseId",
+                table: "NotionUserSettings");
+        }
+    }
+}
