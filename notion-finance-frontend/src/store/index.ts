2b5f5bf267226d6ce3de399d05ff7a94922ac36f
Yunus Emre ÖRCÜN <yunus.emre.orcun@outlook.com>
2022-01-25 21:56:52 +0300

    Disable authorization button in homepage

--- notion-finance-frontend/src/store/index.ts
+++ notion-finance-frontend/src/store/index.ts
@@ -1,12 +1,14 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
         jwtToken: "",
-        expirationDate: new Date()
+        expirationDate: new Date(),
+        authorizationUrl: ""
     },
     mutations: {
         setJwtToken(state, payload) {
@@ -15,7 +17,13 @@ export default new Vuex.Store({
         },
         removeJwtToken(state) {
             state.jwtToken = "";
+        },
+        saveAuthorizationUrl(state, payload) {
+            state.authorizationUrl = payload.url;
         }
+    },
+    getters: {
+        loggedIn: state => !!state.jwtToken
     },
     actions: {},
     modules: {},
--- notion-finance-frontend/src/views/Home.vue
+++ notion-finance-frontend/src/views/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="section columns is-centered is-vcentered">
+    <div v-if="$store.getters.loggedIn" class="column is-half">
+      <b-button :disabled="!urlLoaded" @click="authorizeNotion">Authorize Notion</b-button>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios"; // @ is an alias to /src
 
 @Component({
   components: {
     HelloWorld,
   },
+  data() {
+    return {
+      urlLoaded: false
+    }
+  },
+  methods: {
+    getAuthorizationUrl() {
+      if (!this.$store.getters.loggedIn) return;
+      axios
+        .get("http://localhost:7047/api/notion/get-authorization-url", {
+          headers: {
+            "Authorization": `Bearer ${this.$store.state.jwtToken}`
+          }
+        })
+      .catch(reason => {
+        console.log("Error getting authorization url", reason);
+        this.urlLoaded = false;
+      })
+      .then(response => {
+        let url = response.data;
+        this.$store.commit("saveAuthorizationUrl", {url});
+        this.urlLoaded = true;
+        console.log("Saved auth url");
+      })
+    },
+    authorizeNotion() {
+      let win = window.open(this.$store.state.authorizationUrl);
+      if (win) console.log("Started auth")
+      else this.$buefy.notification.open({
+        message: "Please allow popups for this website",
+        type: "is-danger"
+      })
+    }
+  },
+  created() {
+    this.getAuthorizationUrl();
+  }
 })
 export default class Home extends Vue {}
 </script>
